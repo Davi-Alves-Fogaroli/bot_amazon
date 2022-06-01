931dfd2daf53bf1c174a317b86d8d75c8cbf8052
Davi <davi.a.fogaroli@gmail.com>
2022-06-01 13:39:37 -0300

    bug fix, code review

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 # lIBS
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import json
-import os
+# import os
 # EXCEPTIONS
 from selenium.common.exceptions import SessionNotCreatedException
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
 # WEBDRIVER
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
@@ -17,7 +13,7 @@ from selenium import webdriver
 
 print("....Start....")
 
-load_dotenv()
+# load_dotenv()
 
 # configure and define webDriver
 options = Options()
@@ -25,7 +21,7 @@ options = Options()
 
 try :
     # define driver and accesse website
-    s = Service(os.environ.get("DRIVER_PATCH"))
+    s = Service("driver\chromedriver_101.exe")
     driver = webdriver.Chrome(service=s, options=options)
     driver.get("https://www.amazon.com.br/")
     driver.maximize_window()
@@ -36,46 +32,45 @@ except SessionNotCreatedException :
 # locate the search bar, input the specific key and search for...
 driver.find_element(By.ID, "twotabsearchtextbox").send_keys("iphone" + Keys.ENTER)
 
-# wait page load
-#WebDriverWait(driver, timeout=20).until(EC.presence_of_element_located((By.XPATH, "sg-col-inner")))
-
-
 # find all products cards and some garbage
-cards = driver.find_elements(By.CLASS_NAME, "sg-col-inner")
-#print("cards_______:",cards)
+card = driver.find_elements(By.CSS_SELECTOR, "div[data-asin]")
 
 count = 1
 dic= {}
 file = open('json/data_iphone.json', 'w', encoding ='utf8') 
 list=[]
-# take name and price 
 
-for x in cards:
-    try :
-        # find price
-        price_whole = x.find_element(By.CLASS_NAME, "a-price-whole").text
-        price_fraction = x.find_element(By.CLASS_NAME, "a-price-fraction").text
-        price = "R$"+price_whole+","+price_fraction
-        #print(f"{count}_price:", price)
-
-        # find name
-        #name = x.find_element(By.XPATH, ".//span[@class='a-size-base a-color-base a-text-normal']").text
-        name = WebDriverWait(x, timeout=0.5).until(EC.visibility_of_element_located((By.XPATH, ".//span[@class='a-size-base a-color-base a-text-normal']")))
-        name = name.text
-        print(name,"\n")
-
-        # temporary storage
-        dic[name]= price
-        #print("_"*100,dic)
-
-    except NoSuchElementException:
-        print(f"{count}_element not found", "\n")
-        count+=1
+for x,y in enumerate(card):
+   
+    list_text1 = y.text
+    list_text1_split = list_text1.split("\n")
+    print("--"*30,"\n",list_text1.split("\n"))
     
-    except TimeoutException:
-        print(f"{count}_search time out", "\n")
-        count+=1
-    count+=1
+    if len(list_text1_split) < 4 :
+        continue
+    
+    list2 = ["Oferta","Patrocinado", "PESQUISAS RELACIONADAS", "PRECISA DE AJUDA?", "Nenhuma oferta em destaque disponível"]
+
+    list_boll=[x in y for x in list2 for y in list_text1_split]
+    
+    if any(list_boll):
+        continue
+
+    indice_name = 0
+    indice_price = 2
+    
+    if list_text1_split[0] == "Patrocinados": 
+        indice_price += 1
+        indice_name += 1
+    
+    if "Economize" in list_text1_split[2] : indice_price += 1
+    
+    # take name and price
+    name = list_text1_split[indice_name]
+    price = list_text1_split[indice_price]
+    
+    # temporary storage
+    dic[name]= price
     
 # final storage
 list.append(dic)
